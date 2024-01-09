0464722bf4907799157e98a0d1110681daa263bf
Stephanie <syng828@gmail.com>
2024-01-09 14:23:53 -0800

    added seed

--- hash.py
+++ hash.py
@@ -1,15 +1,19 @@
 import base64
 import hashlib
+import secrets
 
 
 def create_alias(url: str):
-    url_bytes = url.encode('utf-8')
-    hasher = hashlib.sha256(url_bytes)
+    seed = secrets.token_bytes(16)  # generates a random 16-byte value
+    # adds to url to ensure its unique
+    data_to_hash = seed + url.encode('utf-8')
+    hasher = hashlib.sha256(data_to_hash)
     hash_bytes = hasher.digest()
     alias = base64.urlsafe_b64encode(
         hash_bytes).decode('utf-8')[:5]
     return alias
 
 
+print(create_alias("https://youtube.com"))
 print(create_alias("https://youtube.com"))
 print(create_alias("https://google.com"))
