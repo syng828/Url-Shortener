209205cb78b605ecae9e7b8b33561899500431ee
Stephanie <syng828@gmail.com>
2024-01-10 22:19:00 -0800

    changed formatting

--- args.py
+++ args.py
@@ -1,16 +1,24 @@
 import argparse
-import time
 
 
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "database_file", help="Database file name (Required)")
+        "--database_file",
+        help="Database file name",
+        required=True,
+        type=str)
     parser.add_argument(
-        "--host", help="Host for the server", default="localhost")
-    parser.add_argument("--port", help="Port for the server",
-                        default=5000, type=int)
-    return parser.parse_args()
+        "--host",
+        help="Host for the server",
+        default="localhost",
+        type=str)
+    parser.add_argument(
+        "--port",
+        help="Port for the server",
+        default=5000,
+        type=int)
+    print(parser.parse_args())
 
 
 if __name__ == "__main__":
