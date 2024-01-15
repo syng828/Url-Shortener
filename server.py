b2a0674d518b448820f7db7a4570800379b21afb
Stephanie <syng828@gmail.com>
2024-01-14 16:07:33 -0800

    added logging

--- server.py
+++ server.py
@@ -6,6 +6,8 @@ from fastapi.testclient import TestClient
 from hash import create_alias
 from args import get_args
 from enums import HttpStatus, code_to_enum
+import logging
+import time
 
 app = FastAPI()
 
@@ -20,24 +22,36 @@ async def create_url(request: Request):
         data = await request.json()
         url = data.get("url", None)
         alias = data.get("alias", None)
+        logging.debug(f"Received data: {data}")
+
         if (url is None):  # url not provided
             raise ValueError("Url not provided.")
+
         if (args.disable_random_alias):  # if random alias is disabled
             if (alias is None):
                 raise ValueError("Alias is required.")
+
         if (helpers.alias_exists(DATABASE, alias)):  # alias exists
             raise HTTPException(
                 status_code=HttpStatus.INVALID.code, detail="Alias already exists.")
+
         if (alias is None):  # creates random alias if not given.
+            logging.debug("Generating random alias.")
             alias = create_alias(url)
+
         helpers.insert_url(DATABASE, url, alias)
+        logging.info(f"URL created successfully: {url}, Alias: {alias}")
         return {"url": url, "alias": alias}
+
     except ValueError as e:
+        logging.error(f"ValueError: {str(e)}")
         raise HTTPException(
             status_code=HttpStatus.BAD_REQUEST.code, detail=str(e))
     except HTTPException as e:
+        logging.error(f"HTTPException: {str(e.detail)}")
         raise e
     except Exception as e:
+        logging.exception(f"Internal server error: {str(e)}")
         raise HTTPException(
             status_code=HttpStatus.INTERNAL_SERVER_ERROR.code, detail=str(e))
 
@@ -48,6 +62,7 @@ def list_all():
         url_list = helpers.list_alias_url(DATABASE)
         return {"url_list": url_list}
     except Exception as e:
+        logging.exception("Unable to list the urls and aliases")
         raise HTTPException(
             status_code=HttpStatus.INTERNAL_SERVER_ERROR.code, detail=str(e))
 
@@ -58,9 +73,11 @@ def find_alias(alias: str):
         target_url = helpers.alias_to_url(DATABASE, alias)
         return RedirectResponse(url=target_url)
     except ValueError as e:
+        logging.error("Alias not found in find alias")
         raise HTTPException(
             status_code=HttpStatus.NOT_FOUND.code, detail="Alias not found.")
     except Exception as e:
+        logging.exception(f"Internal server error while finding alias")
         raise HTTPException(
             status_code=HttpStatus.INTERNAL_SERVER_ERROR.code, detail=str(e))
 
@@ -73,9 +90,11 @@ def delete_alias(alias: str):
         else:
             raise KeyError("Alias not found")
     except KeyError as e:
+        logging.error("Alias not found in delete alias")
         raise HTTPException(
             status_code=HttpStatus.NOT_FOUND.code, detail=str(e))
     except Exception as e:
+        logging.exception(f"Internal server error while deleting alias")
         raise HTTPException(
             status_code=HttpStatus.INTERNAL_SERVER_ERROR.code, detail=str(e))
 
@@ -87,6 +106,14 @@ async def http_exception_handler(request, exception):
     status_description = status_enum.description
 
     return HTMLResponse(content=status_description, status_code=status_code)
+
+logging.Formatter.converter = time.gmtime
+
+logging.basicConfig(
+    format="%(asctime)s.%(msecs)03dZ %(levelname)s:%(name)s:%(message)s",
+    datefmt="%Y-%m-%dT%H:%M:%S",
+    level=logging.ERROR - (args.verbose*10),
+)
 
 if __name__ == "__main__":
     uvicorn.run("server:app", port=8000, reload=True)
